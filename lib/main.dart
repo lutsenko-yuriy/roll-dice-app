653393dfe5a4d91b8de2d74f4b127b32380dc926
Yuriy Lutsenko <lutsenko.yuriy93@gmail.com>
2024-02-14 21:35:05 +0100

    Take GradientContainer into a separate file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:roll_dice_app/gradient_container.dart';
 
 void main() {
   runApp(
@@ -8,30 +9,4 @@ void main() {
       ),
     ),
   );
-}
-
-class GradientContainer extends StatelessWidget {
-  const GradientContainer({key}): super(key: key);
-
-  @override
-  Widget build(context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [Colors.orange, Colors.blue],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        ),
-      ),
-      child: const Center(
-        child: Text(
-          "Hello world!",
-          style: TextStyle(
-              color: Color.fromARGB(255, 0, 51, 92),
-              fontSize: 36,
-              fontWeight: FontWeight.bold),
-        ),
-      ),
-    );
-  }
 }
