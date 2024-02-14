b9bbbd351efce96842b4ebf4e94681e83a7f0f50
Yuriy Lutsenko <lutsenko.yuriy93@gmail.com>
2024-02-14 21:32:05 +0100

    Add new widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,28 +2,36 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [Colors.orange, Colors.blue],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              "Hello world!",
-              style: TextStyle(
-                color: Colors.deepPurple,
-                fontSize: 36,
-                fontWeight: FontWeight.bold
-              ),
-            ),
-          ),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
+}
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({key}): super(key: key);
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Colors.orange, Colors.blue],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          "Hello world!",
+          style: TextStyle(
+              color: Color.fromARGB(255, 0, 51, 92),
+              fontSize: 36,
+              fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
 }
